23ec4304d2bba077d805762ec8f5d4addb10389c
Sheena Nickerson <sheenanick@gmail.com>
2017-01-27 11:54:09 -0800

    add photo stream to User model

--- app/src/main/java/com/lifehackig/realm/model/User.java
+++ app/src/main/java/com/lifehackig/realm/model/User.java
@@ -1,5 +1,6 @@
 package com.lifehackig.realm.model;
 
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
@@ -12,6 +13,7 @@ public class User extends RealmObject{
     private String uid;
     private String username;
     private byte[] profileImage;
+    private RealmList<Photo> photoStream;
 
     public void setUsername(String username) {
         this.username = username;
@@ -31,6 +33,10 @@ public class User extends RealmObject{
 
     public byte[] getProfileImage() {
         return profileImage;
+    }
+
+    public RealmList<Photo> getPhotoStream() {
+        return photoStream;
     }
 
 }
